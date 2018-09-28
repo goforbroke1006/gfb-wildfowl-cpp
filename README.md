3f346d3a9d59bc508f4906e31ff53aee63b93835
GoForBroke <go.for.broke1006@gmail.com>
2018-09-28 16:17:39 +0300

    update guide

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # gfb-wildfowl-cpp
 
 Cpp shared library for tests.
+
+
+#### How to install shared lib
+
+```bash
+sudo chmod +x setup.sh
+./setup.sh
+```
+
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BUILD_DIR=./cmake-build-debug
+
+mkdir -p ${BUILD_DIR}
+cmake --build ${BUILD_DIR} --target all
+cd ${BUILD_DIR}
+make
+sudo make install
